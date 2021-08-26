2e4dfa75a4191f6b1e3b5d9227ed5ea00f75e02a
apurvaoza <apurva.oza.space@gmail.com>
2021-08-26 12:52:48 -0700

    created functions.py with sections of physics subfield

---
+++ functions.py
@@ -0,0 +1,35 @@
+from universal import *
+"""
+@author: Apurva V Oza, JPL/Caltech ; u^b, 2021
+email : apurva.oza.space@gmail.com
+MODULE FUNCTION: # Functions from fundamental physics textbooks 
+Book references: 
+"Ellipsoidal Figures of Equilibrium" by Subrahmanyan Chandrasekhar , 1969
+"Radiative Processes in Astrophysics" by Rybicki & Lightman, 1979
+"Energetic Charged-Particle Interactions with Atmospheres and Surfaces" by Robert E. Johnson , University of Virginia/ New York University 1990.
+"Theoretical Astrophysics" by Phil Arras, University of Virginia . 2012
+
+Recipe: You'll need this spice to get your modules to function.
+"""
+
+#####
+# Tidal physics
+######
+
+############# orbital and dynamical quantities #############################
+
+###
+# Exosphere (collisionless atmosphere) physics
+####
+
+###
+# Radial structure
+####
+
+
+###########
+#  Areas of a plasma torus and occulting dust cloud. Assuming it is tau > 1 at continuum wavelengths
+############
+
+##radiation pressure ##
+
